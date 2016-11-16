9ea3439f444ddbe997be2d76ec075f8699200f05
Palakis <contact@slepin.fr>
2016-11-16 13:52:12 +0100

    License change

--- forms/settings-dialog.cpp
+++ forms/settings-dialog.cpp
@@ -1,3 +1,21 @@
+/*
+obs-websocket
+Copyright (C) 2016	Stéphane Lepin <stephane.lepin@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
 #include <obs-frontend-api.h>
 #include "settings-dialog.h"
 #include "ui_settings-dialog.h"
