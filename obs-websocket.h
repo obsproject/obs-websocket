a9b13572961cfcf3a00d70fd79fdae64ea72bff5
Stéphane Lepin <contact@slepin.fr>
2016-11-19 17:44:09 +0100

    Bump version number

--- obs-websocket.h
+++ obs-websocket.h
@@ -19,6 +19,6 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #ifndef OBSWEBSOCKET_H
 #define OBSWEBSOCKET_H
 
-#define OBS_WEBSOCKET_VERSION "0.2-alpha"
+#define OBS_WEBSOCKET_VERSION "0.3-alpha"
 
 #endif // OBSWEBSOCKET_H
