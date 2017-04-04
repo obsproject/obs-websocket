41a91912234474586a169f45b683d590e6c7c9a6
asquelt <asq@asq.art.pl>
2017-04-04 18:47:21 +0200

    try to build osx version and xenial docker version

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -6,7 +6,6 @@ brew update
 # OBS Studio deps
 brew install ffmpeg
 brew install libav
-brew install x264
 
 # qtwebsockets deps
 brew install qt5
@@ -21,4 +20,3 @@ mkdir build && cd build
 cmake .. -DCMAKE_PREFIX_PATH=$(brew --prefix qt5) -DQt5WebSockets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5WebSockets -DCMAKE_MODULE_PATH=$(brew --prefix qt5)/lib/cmake/ -DLIBOBS_INCLUDE_DIR=~/obs-studio/libobs -DLIBOBS_LIB=~/obs-studio/libobs && make
 sudo make install
 
-sudo ldconfig
