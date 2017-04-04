75e2198315d9f1e3f4d0829637ad8d6a79a29893
konrad rzentarzewski <kondi@hotategai.local>
2017-04-04 22:53:38 +0200

    try to build osx version

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -2,6 +2,12 @@
 set -ex
 
 mkdir build && cd build
-cmake .. -DCMAKE_PREFIX_PATH=$(brew --prefix qt5)/lib/cmake -DQt5WebSockets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5WebSockets -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs -DLIBOBS_LIB=../../obs-studio/libobs -DCMAKE_INSTALL_PREFIX=/usr && make -j4
-
-# /clang_64/lib/cmake/ ?
+ls -lh $(brew --prefix qt5)
+cmake .. \
+  -DCMAKE_PREFIX_PATH=$(brew --prefix qt5) \
+  -DWITH_QT5=1 \
+  -DQt5WebSockets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5WebSockets \
+  -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
+  -DLIBOBS_LIB=../../obs-studio/libobs \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+&& make -j4
