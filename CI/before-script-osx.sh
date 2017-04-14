51430901429f485f930e15b9f92a91de97c39ff3
Palakis <contact@slepin.fr>
2017-04-14 22:38:57 +0200

    Work on OS X Travis builds

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -2,8 +2,7 @@
 set -ex
 
 mkdir build && cd build
-ls -lh $(brew --prefix qt5)
-ls -lhR ../../obs-studio/build
+
 cmake .. \
   -DCMAKE_PREFIX_PATH=$(brew --prefix qt5)/lib/cmake \
   -DQt5Core_DIR=$(brew --prefix qt5)/lib/cmake/Qt5Core \
@@ -11,6 +10,7 @@ cmake .. \
   -DQt5WebSockets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5WebSockets \
   -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
   -DLIBOBS_LIB=../../obs-studio/build/libobs/libobs.dylib \
+  -DOBS_FRONTEND_LIB=../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib \
   -DCMAKE_INSTALL_PREFIX=/usr \
 && make -j4
 
