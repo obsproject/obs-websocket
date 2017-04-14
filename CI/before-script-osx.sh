bffc1d6a1f018f61be13f73baf12e8a3dd48f1c5
asquelt <asq@asq.art.pl>
2017-04-14 15:51:11 +0200

    verifying libobs location

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -3,14 +3,14 @@ set -ex
 
 mkdir build && cd build
 ls -lh $(brew --prefix qt5)
-ls -lh ../../obs-studio/libobs
+ls -lh ../../obs-studio/build/libobs/libobs.*
 cmake .. \
   -DCMAKE_PREFIX_PATH=$(brew --prefix qt5)/lib/cmake \
   -DQt5Core_DIR=$(brew --prefix qt5)/lib/cmake/Qt5Core \
   -DQt5Widgets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5Widgets \
   -DQt5WebSockets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5WebSockets \
   -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
+  -DLIBOBS_LIB=../../obs-studio/build/libobs/libobs.dylib \
   -DCMAKE_INSTALL_PREFIX=/usr \
 && make -j4
 
-# -DLIBOBS_LIB=../../obs-studio/build/libobs/libobs.dylib \
