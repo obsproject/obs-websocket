f48b31664e84c097ed94de5b3d4b5e0a733e6a7a
Palakis <contact@slepin.fr>
2017-04-15 01:08:48 +0200

    Trying with absolute paths

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -11,7 +11,7 @@ cmake .. \
   -DQt5Widgets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5Widgets \
   -DQt5WebSockets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5WebSockets \
   -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
-  -DOBS_FRONTEND_LIB_DIR=../../obs-studio/build/UI/obs-frontend-api \
+  -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_INSTALL_PREFIX=/usr \
 && make -j4
 
