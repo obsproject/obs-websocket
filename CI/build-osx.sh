16f07ff0c3be0f928fe0c4a22ebbcba637d2816a
Stéphane <contact@slepin.fr>
2017-04-20 13:58:31 +0200

    Travis OS X Packaging: include QtWebSockets in pkg

--- CI/build-osx.sh
+++ CI/build-osx.sh
@@ -4,10 +4,7 @@ set -ex
 mkdir build && cd build
 
 cmake .. \
-  -DCMAKE_PREFIX_PATH=$(brew --prefix qt5)/lib/cmake \
-  -DQt5Core_DIR=$(brew --prefix qt5)/lib/cmake/Qt5Core \
-  -DQt5Widgets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5Widgets \
-  -DQt5WebSockets_DIR=$(brew --prefix qt5)/lib/cmake/Qt5WebSockets \
+  -DQTDIR=$(brew --prefix qt5) \
   -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_INSTALL_PREFIX=/usr \
--- CI/package-osx.sh
+++ CI/package-osx.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export WS_LIB="$(brew --prefix qt5)/lib/QtWebSockets.framework/QtWebSockets"
+
 export GIT_HASH=$(git rev-parse --short HEAD)
 
 export VERSION="$GIT_HASH-$TRAVIS_BRANCH"
@@ -12,6 +14,7 @@ fi
 export FILENAME="obs-websocket-$VERSION-osx.pkg"
 
 mkdir release
+cp $WS_LIB ./release
 
 packagesbuild ./CI/osx/obs-websocket.pkgproj
 
