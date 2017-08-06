9e3ce26ba01293541f6cf881384e47f96953e07b
Stéphane L <contact@slepin.fr>
2017-08-06 21:45:24 +0200

    Revert "macOS CI: install qt5 from Homebrew's index"
    This reverts commit ee1486274db4280f71e6f202c4f4bef1b7f37ba3.

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -7,7 +7,7 @@ brew install ffmpeg
 brew install libav
 
 # qtwebsockets deps
-brew install qt5
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/fdb7c6e960e830b3bf630850c0002c5df9f68ed8/Formula/qt5.rb
 echo "Qt path: $(find /usr/local/Cellar/qt5 -d 1 | tail -n 1)"
 
 # Build obs-studio
