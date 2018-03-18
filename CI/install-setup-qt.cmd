dc0d5ce98f938bc4fae09a1c36dd8775b10a6775
Stéphane L <contact@slepin.fr>
2018-03-18 23:14:19 +0100

    CI: let's try with VS2015 instead

--- CI/install-setup-qt.cmd
+++ CI/install-setup-qt.cmd
@@ -2,5 +2,5 @@
 
 REM Set default values to use AppVeyor's built-in Qt.
 set QTDIR32=C:\Qt\5.10.1\msvc2015
-set QTDIR64=C:\Qt\5.10.1\msvc2017_64
+set QTDIR64=C:\Qt\5.10.1\msvc2015_64
 set QTCompileVersion=5.10.1
