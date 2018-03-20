0a50e2a95caf773ea615572229ea6bb57f219e54
Stéphane Lepin <stephane.lepin@gmail.com>
2018-03-20 11:49:37 +0100

    ci(linux): use OBS dev deps from PPA release

--- CI/build-xenial.sh
+++ CI/build-xenial.sh
@@ -4,5 +4,5 @@ set -ex
 cd /root/obs-websocket
 
 mkdir build && cd build
-cmake -DLIBOBS_INCLUDE_DIR="../../obs-studio/libobs" -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j4
--- CI/install-dependencies-xenial.sh
+++ CI/install-dependencies-xenial.sh
@@ -1,57 +1,18 @@
 #!/bin/sh
 set -ex
 
-# OBS Studio deps
+add-apt-repository -y ppa:obsproject/obs-studio
 apt-get -qq update
-apt-get install -y \
-        libc-dev-bin libc6-dev \
-        git \
-        build-essential
 
 apt-get install -y \
-        build-essential \
-        checkinstall \
-        cmake \
-        libasound2-dev \
-        libavcodec-dev \
-        libavdevice-dev \
-        libavfilter-dev \
-        libavformat-dev \
-        libavutil-dev \
-        libcurl4-openssl-dev \
-        libfontconfig-dev \
-        libfreetype6-dev \
-        libgl1-mesa-dev \
-        libjack-jackd2-dev \
-        libjansson-dev \
-        libpulse-dev \
-        libqt5x11extras5-dev \
-        libspeexdsp-dev \
-        libswresample-dev \
-        libswscale-dev \
-        libudev-dev \
-        libv4l-dev \
-        libvlc-dev \
-        libx11-dev \
-        libx264-dev \
-        libxcb-shm0-dev \
-        libxcb-xinerama0-dev \
-        libxcomposite-dev \
-        libxinerama-dev \
-        pkg-config \
-        qtbase5-dev
+	libc-dev-bin \
+	libc6-dev git \
+	build-essential \
+	cmake \
+	obs-studio \
+	libqt5websockets5-dev
 
-# obs-websocket deps
-apt-get install -y libqt5websockets5-dev
-
-# Build obs-studio
-cd /root
-git clone https://github.com/jp9000/obs-studio ./obs-studio
-cd obs-studio
-git checkout 21.0.0
-mkdir build && cd build
-cmake -DUNIX_STRUCTURE=1 -DCMAKE_INSTALL_PREFIX=/usr ..
-make -j4
-make install
+# Dirty hack
+wget -O /usr/include/obs/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/master/UI/obs-frontend-api/obs-frontend-api.h
 
 ldconfig
