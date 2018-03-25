33e4017822506de6e26d5ef3d3bf1578402d9b6c
Ryan Foster <RytoEX@gmail.com>
2018-03-25 01:31:51 -0400

    CI: Build as RelWithDebInfo instead of Release
    OBS official builds are produced with RelWithDebInfo. This will produce
    a PDB file for the plugin, similar to the native OBS plugins.

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -9,5 +9,6 @@ cmake .. \
   -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
   -DLIBOBS_LIB=../../obs-studio/libobs \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_INSTALL_PREFIX=/usr \
 && make -j4
