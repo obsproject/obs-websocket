8a40f355c86847f364c2ecf5d3abab03f6cd0d90
Stéphane Lepin <stephane.lepin@gmail.com>
2018-12-23 18:21:31 +0100

    sources: fix indents again

--- src/WSRequestHandler.h
+++ src/WSRequestHandler.h
@@ -143,7 +143,7 @@ class WSRequestHandler : public QObject {
 		static void HandleGetTextGDIPlusProperties(WSRequestHandler* req);
 
 		static void HandleSetTextFreetype2Properties(WSRequestHandler* req);
-    	static void HandleGetTextFreetype2Properties(WSRequestHandler* req);
+		static void HandleGetTextFreetype2Properties(WSRequestHandler* req);
 
 		static void HandleSetBrowserSourceProperties(WSRequestHandler* req);
 		static void HandleGetBrowserSourceProperties(WSRequestHandler* req);
