b14b18e4be45a3fd8a9f8b794d9f95012dc7d2b3
Stéphane L <contact@slepin.fr>
2019-03-17 14:45:41 +0100

    ci(macos): setup Azure Pipelines

--- CI/package-macos.sh
+++ CI/package-macos.sh
@@ -13,13 +13,10 @@ echo "[obs-websocket] Preparing package build"
 export QT_CELLAR_PREFIX="$(/usr/bin/find /usr/local/Cellar/qt -d 1 | sort -t '.' -k 1,1n -k 2,2n -k 3,3n | tail -n 1)"
 
 export GIT_HASH=$(git rev-parse --short HEAD)
+export GIT_BRANCH_OR_TAG=$(git name-rev --name-only HEAD | awk -F/ '{print $NF}')
 
-export VERSION="$GIT_HASH-$TRAVIS_BRANCH"
-export LATEST_VERSION="$TRAVIS_BRANCH"
-if [ -n "${TRAVIS_TAG}" ]; then
-	export VERSION="$TRAVIS_TAG"
-	export LATEST_VERSION="$TRAVIS_TAG"
-fi
+export VERSION="$GIT_HASH-$GIT_BRANCH_OR_TAG"
+export LATEST_VERSION="$GIT_BRANCH_OR_TAG"
 
 export FILENAME="obs-websocket-$VERSION.pkg"
 export LATEST_FILENAME="obs-websocket-latest-$LATEST_VERSION.pkg"
