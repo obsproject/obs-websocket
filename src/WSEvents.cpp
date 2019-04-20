e0d33a9683c1fbe7be059117ac39fd01bf6648ef
Stéphane L <contact@slepin.fr>
2019-04-20 23:09:59 +0200

    events: remove static_cast

--- src/WSEvents.cpp
+++ src/WSEvents.cpp
@@ -130,7 +130,7 @@ WSEvents::~WSEvents() {
 }
 
 void WSEvents::FrontendEventHandler(enum obs_frontend_event event, void* private_data) {
-	WSEvents* owner = static_cast<WSEvents*>(private_data);
+	auto owner = reinterpret_cast<WSEvents*>(private_data);
 
 	if (!owner->_srv) {
 		return;
