83382d8dcfcad2d6ade10fcdf6a4e22d4ee7e996
Stéphane L <contact@slepin.fr>
2019-05-11 17:13:41 +0200

    general: remove boost remains

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -20,7 +20,6 @@ echo "[obs-websocket] Building 'obs-websocket' for macOS."
 mkdir -p build && cd build
 cmake .. \
 	-DQTDIR=/usr/local/opt/qt \
-	-DBOOST_ROOT=/usr/local/opt/boost \
 	-DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
 	-DLIBOBS_LIB=../../obs-studio/libobs \
 	-DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
