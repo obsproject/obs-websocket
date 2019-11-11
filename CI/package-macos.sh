7dd8cb5d8d20d7b5892df878857efa4b001c5ae5
Stéphane Lepin <stephane.lepin@gmail.com>
2019-11-11 15:27:27 +0100

    ci(macos): use framework rpath in package script

--- CI/package-macos.sh
+++ CI/package-macos.sh
@@ -23,9 +23,12 @@ export LATEST_FILENAME="obs-websocket-latest-$LATEST_VERSION.pkg"
 
 echo "[obs-websocket] Modifying obs-websocket.so"
 install_name_tool \
-	-change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets \
-	-change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui \
-	-change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore \
+	-add_rpath @executable_path/../Frameworks/QtWidgets.framework/Versions/5/ \
+	-add_rpath @executable_path/../Frameworks/QtGui.framework/Versions/5/ \
+	-add_rpath @executable_path/../Frameworks/QtCore.framework/Versions/5/ \
+	-change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets @rpath/QtWidgets \
+	-change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui @rpath/QtGui \
+	-change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore @rpath/QtCore \
 	./build/obs-websocket.so
 
 # Check if replacement worked
