062216a9e7a6e5983882bcd1cfbe2aab4b4ff802
Stéphane Lepin <stephane.lepin@gmail.com>
2020-02-26 16:48:13 +0100

    ci(windows): fix command

--- CI/download-obs-deps.cmd
+++ CI/download-obs-deps.cmd
@@ -1,2 +1,2 @@
 if not exist %DepsBasePath%.zip curl -kLO https://obsproject.com/downloads/dependencies2017.zip -f --retry 5 -C -
-7z x %DepsBasePath%.zip -o %DepsBasePath%
+7z x %DepsBasePath%.zip -o%DepsBasePath%
