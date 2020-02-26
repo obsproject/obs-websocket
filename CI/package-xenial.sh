e12a0891897c4ac61d28eb0a9d62da03f9078187
Stéphane Lepin <stephane.lepin@gmail.com>
2020-02-26 15:51:16 +0100

    ci: fix codefactor warning (again)

--- CI/package-xenial.sh
+++ CI/package-xenial.sh
@@ -5,7 +5,7 @@ set -e
 export GIT_HASH=$(git rev-parse --short HEAD)
 export PKG_VERSION="1-$GIT_HASH-$BRANCH_SHORT_NAME-git"
 
-if [ "$BRANCH_FULL_NAME" =~ "^refs/tags/" ]; then
+if [[ "$BRANCH_FULL_NAME" =~ "^refs/tags/" ]]; then
 	export PKG_VERSION="$BRANCH_SHORT_NAME"
 fi
 
