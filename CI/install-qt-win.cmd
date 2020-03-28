57896d785dbab1f628b1343691e009774c51679c
Stéphane Lepin <stephane.lepin@gmail.com>
2020-03-28 16:51:37 +0100

    ci(windows): add dependency caching

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,3 +1,8 @@
-if exist Qt_5.10.1.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -)
-7z x Qt_5.10.1.7z -o%QtBaseDir%
+if exist %QtBaseDir% (
+	curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z
+    7z x Qt_5.10.1.7z -o%QtBaseDir%
+) else (
+	echo "Qt is already installed. Download skipped."
+)
+
 dir %QtBaseDir%
