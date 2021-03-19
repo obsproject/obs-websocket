4505612f7528a81163949c357955ed161641e244
tt2468 <tt2468@gmail.com>
2021-03-18 22:51:06 -0700

    CI: Update Qt and OBS deps on Windows
    - Update Qt to 5.15
    - Update OBS dependencies to 2019

--- CI/download-obs-deps.cmd
+++ CI/download-obs-deps.cmd
@@ -1,6 +1,7 @@
-if not exist %DepsBasePath% (
-    curl -o %DepsBasePath%.zip -kLO https://obsproject.com/downloads/dependencies2017.zip -f --retry 5 -C -
-    7z x %DepsBasePath%.zip -o%DepsBasePath%
+if exist %DepsBasePath% (
+    echo "OBS dependencies found. Download skipped."
 ) else (
-    echo "OBS dependencies are already there. Download skipped."
+    echo "OBS dependencies not found. Downloading..."
+    curl -o %DepsBasePath%.zip -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2019.zip -f --retry 5 -C -
+    7z x %DepsBasePath%.zip -o%DepsBasePath%
 )
--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,8 +1,8 @@
-if not exist %QtBaseDir% (
-	curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z
-    7z x Qt_5.10.1.7z -o%QtBaseDir%
+if exist %QtBaseDir% (
+    echo "Qt directory found. Download skipped."
 ) else (
-	echo "Qt is already installed. Download skipped."
+    echo "Qt directory not found. Downloading..."
+    curl -kLO http://home.tt2468.net:86/dl/Qt_5.15.2.7z -f --retry 5 -C -
+    7z x Qt_5.15.2.7z -o%QtBaseDir%
 )
-
 dir %QtBaseDir%
