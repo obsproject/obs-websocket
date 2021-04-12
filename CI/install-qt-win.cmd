30e2a5428fe41897c8e304621e0a3a0b99a5ccb7
tt2468 <tt2468@gmail.com>
2021-04-11 20:21:18 -0700

    CI: Update Qt windows installer download

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -2,7 +2,7 @@ if exist %QtBaseDir% (
     echo "Qt directory found. Download skipped."
 ) else (
     echo "Qt directory not found. Downloading..."
-    curl -kLO http://home.tt2468.net:86/dl/Qt_5.15.2.7z -f --retry 5 -C -
+    curl -kLO https://tt2468.net/dl/Qt_5.15.2.7z -f --retry 5 -C -
     7z x Qt_5.15.2.7z -o%QtBaseDir%
 )
 dir %QtBaseDir%
