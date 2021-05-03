c2b717cabf794f7d2e0543313d0c3fa1abff7703
tt2468 <tt2468@gmail.com>
2021-05-03 13:43:59 -0700

    RequestStatus: Put into namespace
    Apparently this is supposed to help?

--- src/requesthandler/rpc/RequestResult.cpp
+++ src/requesthandler/rpc/RequestResult.cpp
@@ -1,6 +1,6 @@
 #include "RequestResult.h"
 
-RequestResult::RequestResult(RequestStatus statusCode, json responseData, std::string comment) :
+RequestResult::RequestResult(RequestStatus::RequestStatus statusCode, json responseData, std::string comment) :
 	StatusCode(statusCode),
 	ResponseData(responseData),
 	Comment(comment)
@@ -12,7 +12,7 @@ RequestResult RequestResult::Success(json responseData)
 	return RequestResult(RequestStatus::Success, responseData, "");
 }
 
-RequestResult RequestResult::Error(RequestStatus statusCode, std::string comment)
+RequestResult RequestResult::Error(RequestStatus::RequestStatus statusCode, std::string comment)
 {
 	return RequestResult(statusCode, nullptr, comment);
 }
--- src/requesthandler/rpc/RequestResult.h
+++ src/requesthandler/rpc/RequestResult.h
@@ -5,10 +5,10 @@
 
 struct RequestResult
 {
-	RequestResult(RequestStatus statusCode = RequestStatus::Success, json responseData = nullptr, std::string comment = "");
+	RequestResult(RequestStatus::RequestStatus statusCode = RequestStatus::Success, json responseData = nullptr, std::string comment = "");
 	static RequestResult Success(json responseData = nullptr);
-	static RequestResult Error(RequestStatus statusCode, std::string comment = "");
-	RequestStatus StatusCode;
+	static RequestResult Error(RequestStatus::RequestStatus statusCode, std::string comment = "");
+	RequestStatus::RequestStatus StatusCode;
 	json ResponseData;
 	std::string Comment;
 };
