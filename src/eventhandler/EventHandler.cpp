89046ccb1a5a6af40a61e13aa97d25217cac9c00
tt2468 <tt2468@gmail.com>
2021-05-10 17:34:40 -0700

    EventHandler: Add handling of all frontent events we care about

--- src/eventhandler/EventHandler.cpp
+++ src/eventhandler/EventHandler.cpp
@@ -16,5 +16,76 @@ EventHandler::~EventHandler()
 }
 
 void EventHandler::OnFrontendEvent(enum obs_frontend_event event, void *private_data) {
-	auto owner = reinterpret_cast<EventHandler*>(private_data);
+	auto eventHandler = reinterpret_cast<EventHandler*>(private_data);
+
+	switch (event) {
+		// General
+		case OBS_FRONTEND_EVENT_FINISHED_LOADING:
+			break;
+		case OBS_FRONTEND_EVENT_EXIT:
+			break;
+		case OBS_FRONTEND_EVENT_STUDIO_MODE_ENABLED:
+			break;
+		case OBS_FRONTEND_EVENT_STUDIO_MODE_DISABLED:
+			break;
+
+		// Config
+		case OBS_FRONTEND_EVENT_SCENE_COLLECTION_CHANGED:
+			break;
+		case OBS_FRONTEND_EVENT_SCENE_COLLECTION_LIST_CHANGED:
+			break;
+		case OBS_FRONTEND_EVENT_PROFILE_CHANGED:
+			break;
+		case OBS_FRONTEND_EVENT_PROFILE_LIST_CHANGED:
+			break;
+
+		// Scenes
+		case OBS_FRONTEND_EVENT_SCENE_CHANGED:
+			break;
+		case OBS_FRONTEND_EVENT_SCENE_LIST_CHANGED:
+			break;
+		case OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED:
+			break;
+
+		// Transitions
+		case OBS_FRONTEND_EVENT_TRANSITION_CHANGED:
+			break;
+		case OBS_FRONTEND_EVENT_TRANSITION_LIST_CHANGED:
+			break;
+		case OBS_FRONTEND_EVENT_TRANSITION_DURATION_CHANGED:
+			break;
+
+		// Outputs
+		case OBS_FRONTEND_EVENT_STREAMING_STARTING:
+			break;
+		case OBS_FRONTEND_EVENT_STREAMING_STARTED:
+			break;
+		case OBS_FRONTEND_EVENT_STREAMING_STOPPING:
+			break;
+		case OBS_FRONTEND_EVENT_STREAMING_STOPPED:
+			break;
+		case OBS_FRONTEND_EVENT_RECORDING_STARTING:
+			break;
+		case OBS_FRONTEND_EVENT_RECORDING_STARTED:
+			break;
+		case OBS_FRONTEND_EVENT_RECORDING_STOPPING:
+			break;
+		case OBS_FRONTEND_EVENT_RECORDING_STOPPED:
+			break;
+		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTING:
+			break;
+		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED:
+			break;
+		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPING:
+			break;
+		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPED:
+			break;
+		case OBS_FRONTEND_EVENT_VIRTUALCAM_STARTED:
+			break;
+		case OBS_FRONTEND_EVENT_VIRTUALCAM_STOPPED:
+			break;
+
+		default:
+			break;
+	}
 }
