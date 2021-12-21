527a0080027cf8b279ca16b256c61f1ee2141268
tt2468 <tt2468@gmail.com>
2021-12-21 01:33:16 -0800

    RequestHandler: Remove OutputStartFailed request status
    Accidentally pushed this commit when it was for a component which will
    not be implemented.

--- src/requesthandler/types/RequestStatus.h
+++ src/requesthandler/types/RequestStatus.h
@@ -262,17 +262,6 @@ namespace RequestStatus {
 		* @api enums
 		*/
 		StudioModeNotActive = 506,
-		/**
-		* Starting the output failed.
-		*
-		* @enumIdentifier OutputStartFailed
-		* @enumValue 507
-		* @enumType RequestStatus
-		* @rpcVersion -1
-		* @initialVersion 5.0.0
-		* @api enums
-		*/
-		OutputStartFailed = 507,
 		
 
 		/**
