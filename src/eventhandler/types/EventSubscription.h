c8d0ed21386164e012bce55a218a7922426320f1
tt2468 <tt2468@gmail.com>
2022-06-07 21:45:41 -0700

    docs: Fix EventSubscription::All subscribes

--- src/eventhandler/types/EventSubscription.h
+++ src/eventhandler/types/EventSubscription.h
@@ -157,13 +157,13 @@ enum EventSubscription {
 		* Helper to receive all non-high-volume events.
 		*
 		* @enumIdentifier All
-		* @enumValue (General | Config | Scenes | Inputs | Transitions | Filters | Outputs | SceneItems | MediaInputs | Vendors)
+		* @enumValue (General | Config | Scenes | Inputs | Transitions | Filters | Outputs | SceneItems | MediaInputs | Vendors | Ui)
 		* @enumType EventSubscription
 		* @rpcVersion -1
 		* @initialVersion 5.0.0
 		* @api enums
 		*/
-	All = (General | Config | Scenes | Inputs | Transitions | Filters | Outputs | SceneItems | MediaInputs | Ui | Vendors),
+	All = (General | Config | Scenes | Inputs | Transitions | Filters | Outputs | SceneItems | MediaInputs | Vendors | Ui),
 	/**
 		* Subscription value to receive the `InputVolumeMeters` high-volume event.
 		*
