2524dc47cfe0768347c79a48fa95923f57be9428
tt2468 <tt2468@gmail.com>
2022-07-02 08:26:58 -0700

    requesthandler: Fix oopsies in OpenVideoMixProjector

--- src/requesthandler/RequestHandler_Ui.cpp
+++ src/requesthandler/RequestHandler_Ui.cpp
@@ -219,7 +219,7 @@ RequestResult RequestHandler::OpenVideoMixProjector(const Request &request)
 {
 	RequestStatus::RequestStatus statusCode;
 	std::string comment;
-	if (!request.ValidateString("mixType", statusCode, comment))
+	if (!request.ValidateString("videoMixType", statusCode, comment))
 		return RequestResult::Error(statusCode, comment);
 
 	std::string videoMixType = request.RequestData["videoMixType"];
@@ -231,8 +231,7 @@ RequestResult RequestHandler::OpenVideoMixProjector(const Request &request)
 	else if (videoMixType == "OBS_WEBSOCKET_VIDEO_MIX_TYPE_MULTIVIEW")
 		projectorType = "Multiview";
 	else
-		return RequestResult::Error(RequestStatus::InvalidRequestField,
-					    "The field `videoMixType` has an invalid enum value.");
+		return RequestResult::Error(RequestStatus::InvalidRequestField, "The field `videoMixType` has an invalid enum value.");
 
 	int monitorIndex = -1;
 	if (request.Contains("monitorIndex")) {
@@ -246,8 +245,7 @@ RequestResult RequestHandler::OpenVideoMixProjector(const Request &request)
 		if (!request.ValidateOptionalString("projectorGeometry", statusCode, comment))
 			return RequestResult::Error(statusCode, comment);
 		if (monitorIndex != -1)
-			return RequestResult::Error(RequestStatus::TooManyRequestFields,
-						    "`monitorIndex` and `projectorGeometry` are mutually exclusive.");
+			return RequestResult::Error(RequestStatus::TooManyRequestFields, "`monitorIndex` and `projectorGeometry` are mutually exclusive.");
 		projectorGeometry = request.RequestData["projectorGeometry"];
 	}
 
@@ -292,8 +290,7 @@ RequestResult RequestHandler::OpenSourceProjector(const Request &request)
 		if (!request.ValidateOptionalString("projectorGeometry", statusCode, comment))
 			return RequestResult::Error(statusCode, comment);
 		if (monitorIndex != -1)
-			return RequestResult::Error(RequestStatus::TooManyRequestFields,
-						    "`monitorIndex` and `projectorGeometry` are mutually exclusive.");
+			return RequestResult::Error(RequestStatus::TooManyRequestFields, "`monitorIndex` and `projectorGeometry` are mutually exclusive.");
 		projectorGeometry = request.RequestData["projectorGeometry"];
 	}
 
