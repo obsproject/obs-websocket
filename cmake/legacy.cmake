6fd18a7ef1ecb149e8444154af1daab61d4241a9
tt2468 <tt2468@gmail.com>
2023-06-08 15:03:52 -0700

    base: Update version to 5.2.3

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -1,4 +1,4 @@
-project(obs-websocket VERSION 5.2.2)
+project(obs-websocket VERSION 5.2.3)
 set(OBS_WEBSOCKET_RPC_VERSION 1)
 
 option(ENABLE_WEBSOCKET "Enable building OBS with websocket plugin" ON)
