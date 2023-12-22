7a1c71bb9636891f789b3ebe9475ee442dfd45f6
Ryan Foster <ryan@obsproject.com>
2023-12-22 16:38:27 -0500

    base: Update version to 5.3.5
    - CMake update, no functional changes

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -1,4 +1,4 @@
-project(obs-websocket VERSION 5.3.4)
+project(obs-websocket VERSION 5.3.5)
 set(OBS_WEBSOCKET_RPC_VERSION 1)
 
 option(ENABLE_WEBSOCKET "Enable building OBS with websocket plugin" ON)
