305afd763d82ef0b5c79a2b8357ef55b5cd121ab
tt2468 <tt2468@gmail.com>
2024-04-22 18:32:51 -0700

    utils: Remove old *AutoRelease definitions
    Now that OBS has been out with the upstream AutoRelease definitions,
    and obs-websocket is also in-tree-only, these are no longer necessary.

--- src/utils/Obs.h
+++ src/utils/Obs.h
@@ -29,36 +29,6 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 inline void ___properties_dummy_addref(obs_properties_t *) {}
 using OBSPropertiesAutoDestroy = OBSRef<obs_properties_t *, ___properties_dummy_addref, obs_properties_destroy>;
 
-#if !defined(OBS_AUTORELEASE)
-inline void ___source_dummy_addref(obs_source_t *) {}
-inline void ___scene_dummy_addref(obs_scene_t *) {}
-inline void ___sceneitem_dummy_addref(obs_sceneitem_t *) {}
-inline void ___data_dummy_addref(obs_data_t *) {}
-inline void ___data_array_dummy_addref(obs_data_array_t *) {}
-inline void ___output_dummy_addref(obs_output_t *) {}
-inline void ___encoder_dummy_addref(obs_encoder_t *) {}
-inline void ___service_dummy_addref(obs_service_t *) {}
-
-inline void ___weak_source_dummy_addref(obs_weak_source_t *) {}
-inline void ___weak_output_dummy_addref(obs_weak_output_t *) {}
-inline void ___weak_encoder_dummy_addref(obs_weak_encoder_t *) {}
-inline void ___weak_service_dummy_addref(obs_weak_service_t *) {}
-
-using OBSSourceAutoRelease = OBSRef<obs_source_t *, ___source_dummy_addref, obs_source_release>;
-using OBSSceneAutoRelease = OBSRef<obs_scene_t *, ___scene_dummy_addref, obs_scene_release>;
-using OBSSceneItemAutoRelease = OBSRef<obs_sceneitem_t *, ___sceneitem_dummy_addref, obs_sceneitem_release>;
-using OBSDataAutoRelease = OBSRef<obs_data_t *, ___data_dummy_addref, obs_data_release>;
-using OBSDataArrayAutoRelease = OBSRef<obs_data_array_t *, ___data_array_dummy_addref, obs_data_array_release>;
-using OBSOutputAutoRelease = OBSRef<obs_output_t *, ___output_dummy_addref, obs_output_release>;
-using OBSEncoderAutoRelease = OBSRef<obs_encoder_t *, ___encoder_dummy_addref, obs_encoder_release>;
-using OBSServiceAutoRelease = OBSRef<obs_service_t *, ___service_dummy_addref, obs_service_release>;
-
-using OBSWeakSourceAutoRelease = OBSRef<obs_weak_source_t *, ___weak_source_dummy_addref, obs_weak_source_release>;
-using OBSWeakOutputAutoRelease = OBSRef<obs_weak_output_t *, ___weak_output_dummy_addref, obs_weak_output_release>;
-using OBSWeakEncoderAutoRelease = OBSRef<obs_weak_encoder_t *, ___weak_encoder_dummy_addref, obs_weak_encoder_release>;
-using OBSWeakServiceAutoRelease = OBSRef<obs_weak_service_t *, ___weak_service_dummy_addref, obs_weak_service_release>;
-#endif
-
 template<typename T> T *GetCalldataPointer(const calldata_t *data, const char *name)
 {
 	void *ptr = nullptr;
